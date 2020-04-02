e6ebd52a6759f82a43ac3230cda20b7d7facd621
ABIDI Mohamed <mohamed.abidi1105@gmail.com>
2020-04-02 15:23:30 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
-=======
-import React from 'react';
->>>>>>> cf7bf2f5c7e94f8f6629c39989b8c8b1b4133ab8
 import Contact from './components/Contact'
 import './App.css';
 
 
 function App() {
   return (
-<<<<<<< HEAD
 
     <div>
-    <Contact name="Anita Sutton" avatar="https://randomuser.me/api/portraits/women/82.jpg" status />
-    <Contact name="Jim George" avatar="https://randomuser.me/api/portraits/men/85.jpg" />
-    <Contact name="Charlotte Ryan" avatar="https://randomuser.me/api/portraits/women/87.jpg" status />
-=======
-    <div className="App">
-      <Contact />
-     
->>>>>>> cf7bf2f5c7e94f8f6629c39989b8c8b1b4133ab8
+      <Contact name="Anita Sutton" avatar="https://randomuser.me/api/portraits/women/82.jpg" status />
+      <Contact name="Jim George" avatar="https://randomuser.me/api/portraits/men/85.jpg" />
+      <Contact name="Charlotte Ryan" avatar="https://randomuser.me/api/portraits/women/87.jpg" status />
+
     </div>
   )
 
