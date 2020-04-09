fe5bafd8d12345f7fa2073407ff9d1ad50f9777f
ABIDI Mohamed <mohamed.abidi1105@gmail.com>
2020-04-09 13:49:23 +0200

    update Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Contact.css'
+import { render } from '@testing-library/react'
 
 
-function Contact(props) { 
-    
-        return (
-            <div className= "Contact">
-                <img className= "avatar" src = {props.avatar}></img>
-                <div>
-                    <h4 className='name'>{props.name}</h4>
-                    <div className="status">                    
-                        <div className={props.status ? "status-online" : "status-offline"}></div>
-                        <div className="status-text">{props.status ? 'online' : 'offline'}</div>
+class Contact extends React.Component { 
+    constructor(props) {
+        super(props)
+        this.state = {
+            status: false
+        }
+    }
+            render() {
+                return (
+                    <div className= "Contact">
+                        <img className= "avatar" src = {props.avatar}></img>
+                        <div>
+                            <h4 className='name'>{props.name}</h4>
+                            <div className="status">                    
+                                <div className={this.state.status ? "status-online" : "status-offline"} onClick= {event =>{const newStatus = !this.state.status; this.setState({status:newStatus})}}></div>
+                                <div className="status-text">{this.state.status ? 'online' : 'offline'}</div>
+                            </div>
+                        </div>
+
                     </div>
-                </div>
-
-            </div>
-        )
+                )
+                } 
 }
 
 Contact.propTypes = {
